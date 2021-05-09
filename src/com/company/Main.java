cc6513a325b2bd4721d70180db308a7ede461690
phyex0 <phyex0-halit_burak@outlook.com>
2021-05-09 17:50:57 +0300

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+
 
 public class Main {
 
